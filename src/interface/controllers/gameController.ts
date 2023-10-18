3f79180ff2e9577cd67519845b8961cb9d5e241c
Jonathan <jonathan.hedlund.371@gmail.com>
2023-10-18 21:34:34 +0200

    continue implementation of gameController

--- src/interface/controllers/gameController.ts
+++ src/interface/controllers/gameController.ts
@@ -1,18 +1,58 @@
-import { ProjectDependencies } from "../contracts/projectDependencies";
+import { gameUseCases } from "../../application/use-cases/game";
 
-import { createGame } from "../../application/use-cases/game/createGame";
+import { AppError } from "../../shared/appError";
+import { HttpStatusCode } from "../../shared/httpStatusCode";
+
+import type { Request, Response, NextFunction } from "express";
+import type { ProjectDependencies } from "../contracts/projectDependencies";
 
 export const gameController = (dependencies: ProjectDependencies) => {
 	const { gameRepository } = dependencies.databaseService;
 
-	const getAllGames = async () => {
-		const games = await gameRepository.findAll();
-		return games;
+	const getAllGames = (req: Request, res: Response, next: NextFunction) => {
+		const games = gameRepository.findAll();
+		return res.status(HttpStatusCode.OK).json(games);
 	};
 
-	const createGame = async (req: Request, res: Response, next) => {};
+	const createGame = (req: Request, res: Response, next: NextFunction) => {
+		if (!req.body.name) {
+			return next(new AppError(HttpStatusCode.BAD_REQUEST, "Name is required"));
+		}
+		try {
+			const game = gameUseCases.createGame(gameRepository, req.body.name);
+			res.status(HttpStatusCode.CREATED).json(game);
+		} catch (error) {
+			return next(error);
+		}
+	};
+
+	const joinGameById = (req: Request, res: Response, next: NextFunction) => {
+		return next(
+			new AppError(HttpStatusCode.NOT_IMPLEMENTED, "Not implemented")
+		);
+	};
+
+	const makeMoveInGameById = (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		return next(
+			new AppError(HttpStatusCode.NOT_IMPLEMENTED, "Not implemented")
+		);
+	};
+
+	const getGameById = (req: Request, res: Response, next: NextFunction) => {
+		return next(
+			new AppError(HttpStatusCode.NOT_IMPLEMENTED, "Not implemented")
+		);
+	};
 
 	return {
 		getAllGames,
+		createGame,
+		joinGameById,
+		makeMoveInGameById,
+		getGameById,
 	};
 };
