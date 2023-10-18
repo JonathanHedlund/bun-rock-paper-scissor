e7e4339f421c8d511dddfbc0bcf8f40f98ea96a6
Jonathan <jonathan.hedlund.371@gmail.com>
2023-10-18 21:43:53 +0200

    seperate app from server so server does not know about framework

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+import { projectDependencies } from "./config/projectDependencies";
+import { apiRouter } from "./frameworks/routes";
+
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "./shared/appError";
+import { globalErrorHandler } from "./frameworks/error/globalErrorHandler";
+
+const API_VERSION = Bun.env.API_VERSION || "v1";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(`/api/${API_VERSION}`, apiRouter(projectDependencies));
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+	next(new AppError(404, `Can't find ${req.originalUrl} on this server`));
+});
+
+app.use(globalErrorHandler);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,6 @@
-import express from "express";
-
-import { projectDependencies } from "./config/projectDependencies";
-import { apiRouter } from "./frameworks/routes";
-
-import type { Request, Response, NextFunction } from "express";
-import { AppError } from "./shared/appError";
-import { globalErrorHandler } from "./frameworks/error/globalErrorHandler";
+import { app } from "./app";
 
 const PORT = Bun.env.PORT || 3000;
-const API_VERSION = Bun.env.API_VERSION || "v1";
-
-const app = express();
-
-app.use(express.json());
-
-app.use(`/api/${API_VERSION}`, apiRouter(projectDependencies));
-
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
-	next(new AppError(404, `Can't find ${req.originalUrl} on this server`));
-});
-
-app.use(globalErrorHandler);
 
 const server = app.listen(PORT, () => {
 	console.log(`⚡️ Listening on port ${PORT}...`);
