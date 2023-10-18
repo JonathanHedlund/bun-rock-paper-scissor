7e7732a7939ae4824f825e19a8895c0270be4b56
Jonathan <jonathan.hedlund.371@gmail.com>
2023-10-18 21:21:48 +0200

    add http status code enum for clarity

--- src/application/use-cases/game/createGame.ts
+++ src/application/use-cases/game/createGame.ts
@@ -1,12 +1,17 @@
 import { GameStatus } from "../../../entities/gameEntity";
 
-import type { Game } from "../../../entities/gameEntity";
 import { AppError } from "../../../shared/appError";
+import { HttpStatusCode } from "../../../shared/httpStatusCode";
+
+import type { Game } from "../../../entities/gameEntity";
 import type { GameRepository } from "../../contracts/gameRepository";
 
 export const createGame = (gameRepository: GameRepository, name: string) => {
 	if (name.length > 30) {
-		throw new AppError(400, "Name must be less than 30 characters");
+		throw new AppError(
+			HttpStatusCode.BAD_REQUEST,
+			"Name must be less than 30 characters"
+		);
 	}
 
 	const game: Game = {
