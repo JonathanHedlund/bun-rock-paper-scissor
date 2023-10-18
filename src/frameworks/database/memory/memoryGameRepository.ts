f58d78da42090b4ef2e13b743bd31980f81bdf8a
Jonathan <jonathan.hedlund.371@gmail.com>
2023-10-18 20:48:04 +0200

    Remove initial data seed for in memory game repository

--- src/frameworks/database/memory/memoryGameRepository.ts
+++ src/frameworks/database/memory/memoryGameRepository.ts
@@ -1,39 +1,31 @@
+import type { Game } from "../../../entities/gameEntity";
 import type { GameRepository } from "../../../application/contracts/gameRepository";
-import { Game, GameStatus } from "../../../entities/gameEntity";
 
-const games: Game[] = [
-	{
-		id: "1",
-		status: GameStatus.PENDING_PLAYER,
-		players: [{ name: "player1" }],
-	},
-];
+const games: Game[] = [];
 
-export function gameRepositoryMemory(): GameRepository {
-	const findAll = () => {
-		return games;
-	};
-	const findById = (id: string) => {
-		const game = games.find((game) => game.id === id);
-		return game || null;
-	};
-	const add = (game: Game) => {
-		games.push(game);
-	};
-	const updateById = (id: string, game: Game) => {
-		const index = games.findIndex((game) => game.id === id);
-		games[index] = game;
-	};
-	const remove = (id: string) => {
-		const index = games.findIndex((game) => game.id === id);
-		games.splice(index, 1);
-	};
+const findAll = () => {
+	return games;
+};
+const findById = (id: string) => {
+	const game = games.find((game) => game.id === id);
+	return game || null;
+};
+const add = (game: Game) => {
+	games.push(game);
+};
+const updateById = (id: string, game: Game) => {
+	const index = games.findIndex((game) => game.id === id);
+	games[index] = game;
+};
+const remove = (id: string) => {
+	const index = games.findIndex((game) => game.id === id);
+	games.splice(index, 1);
+};
 
-	return {
-		findAll,
-		findById,
-		add,
-		updateById,
-		remove,
-	};
-}
+export const memoryGameRepository: GameRepository = {
+	findAll,
+	findById,
+	add,
+	updateById,
+	remove,
+};
